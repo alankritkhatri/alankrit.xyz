6abad2aab59c5f9e9a7bd110ae20c1ca9ce71a53
Alankrit Khatri <alankritkhatri9@gmail.com>
2024-08-13 17:33:49 +0530

    first commit

---
+++ .history/src/pages/Home/Home_20240723072329.jsx
@@ -0,0 +1,41 @@
+import Infosection from "../../components/Info-Section/Infosection.jsx";
+import "./Home.css";
+import data from "../../alankrit_data/data.json";
+const Home = () => {
+  return (
+    <div className="home">
+      <div
+        class="badge-base LI-profile-badge"
+        data-locale="en_US"
+        data-size="medium"
+        data-theme="dark"
+        data-type="VERTICAL"
+        data-vanity="alankrit-khatri"
+        data-version="v1"
+      >
+        {" "}
+        </div>
+
+      <div className="about-section">
+        <h1 className="name">Alankrit Khatri</h1>
+        <p>
+          Frontend Developer in the making. Looking for frontend internships &
+          projects
+        </p>
+        <div className="dashed-line"></div>
+        <p>
+          I love challenging tasks. I am motivated by interesting projects. I
+          prefer quick-iterations & prototype-driven work. I hate shaving yaks.
+          I love to build.
+        </p>
+      </div>
+      <div className="education-section">
+        <h1>Education</h1>
+        <Infosection {...data} />
+        {console.log(data)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/blog.jsx
@@ -0,0 +1,5 @@
+const Blog = () => {
+  return <div>s</div>;
+};
+
+export default Blog;
